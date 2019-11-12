742168da66486076e98f84702b55e98e0380c2a1
Mick Grove <mick.grove@gmail.com>
2019-11-11 21:15:04 -0800

    merge with upstream

--- build.debug.cmd
+++ build.debug.cmd
@@ -1,2 +1,2 @@
 rem powershell .\build.ps1 -configuration Release
-powershell .\build.ps1 -configuration Release -Target Build-Install-Exe
+powershell .\build.ps1 -configuration Debug -Target Build-Install-Exe
