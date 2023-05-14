516a7eabd9ba0ca3a6a4f32fbbda3c060648098d
N1ckDunn <nick.dunn@nccgroup.com>
2023-05-14 12:45:01 +0100

    Delete Forms directory

--- Forms/frmCodeDetails.vb
+++
@@ -1,4 +0,0 @@
-﻿Public Class frmCodeDetails
-
-
-End Class
--- Forms/frmLoading.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class frmLoading
-    Private Sub frmLoading_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        asAppSettings.AbortCurrentOperation = False
-    End Sub
-
-    Private Sub frmLoading_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
-        asAppSettings.AbortCurrentOperation = True
-    End Sub
-End Class
